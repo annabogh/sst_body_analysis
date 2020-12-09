1d839469fe61676c216975e1896964dda1e4a884
Anna Bøgh <aboegh@me.com>
2020-12-09 11:32:15 +0100

    commented

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
 import pandas as pd
 import numpy as np
+from mpl_toolkits import mplot3d
 import matplotlib.pyplot as plt
+from sklearn.linear_model import LinearRegression
 
 def read_dataset(filepath: str, interpolation_step: float = 1) -> pd.DataFrame:
     """
@@ -28,13 +30,39 @@ def read_dataset(filepath: str, interpolation_step: float = 1) -> pd.DataFrame:
     return interpolated_dataset
 
 def plot_from_xdir(dataset: pd.DataFrame):
+    """
+    Plots polygon from x-direction in northing/height graph.
+    """
     plt.plot(dataset["Northing"], dataset["Height"])
     plt.show()
 
+
+
+def plot_plane_and_layer(dataset: pd.DataFrame, model: LinearRegression):  
+    """
+    Calculates estimated Z-values for plane and plots plane and polygon (AKA layer) in 3D view.
+    """  
+    x_values = [dataset["Easting"].min(), dataset["Easting"].max()] * 2
+    y_values = [dataset["Northing"].max()] * 2 + [dataset["Northing"].min()] * 2
+    #Calculates predicted z values and makes variable.
+    predicted_zvalues = model.predict(X=np.transpose([x_values, y_values]))
+    ax = plt.axes(projection="3d")
+
+    ax.plot3D(dataset["Easting"], dataset["Northing"], dataset["Height"])
+    print(predicted_zvalues.shape)
+    ax.plot_surface(np.array(x_values).reshape((2, 2)), np.array(y_values).reshape((2, 2)), predicted_zvalues.reshape((2, 2)), alpha=0.5)
+    plt.show()
+
 def estimate_plane(dataset: pd.DataFrame):
-    return
+    """
+    Makes flat plane based on polygon points.
+    """
+    model = LinearRegression()
+    model.fit(X=dataset[["Easting", "Northing"]], y=dataset["Height"])
+    plot_plane_and_layer(dataset, model)
 
 if __name__ == "__main__":
     dataset = read_dataset(r"Data\Polygons_Litledalsfjellet\SS_39.asc")
     print(dataset)
-    plot_from_xdir(dataset)
+    #plot_from_xdir(dataset)
+    estimate_plane(dataset)
